c58849d89eaf6d010a6f3ac4ed87d4b757d2a28e
nothuntersolo <aaazzzaaammmff@gmail.com>
2025-10-18 17:12:47 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -194,7 +194,7 @@ else
         Library.DevicePlatform = UserInputService:GetPlatform()
     end)
     Library.IsMobile = (Library.DevicePlatform == Enum.Platform.Android or Library.DevicePlatform == Enum.Platform.IOS)
-    Library.MinSize = Library.IsMobile and Vector2.new(480, 240) or Vector2.new(480, 360)
+    Library.MinSize = Library.IsMobile and Vector2.new(360, 240) or Vector2.new(480, 360)
 end
 
 local Templates = {
